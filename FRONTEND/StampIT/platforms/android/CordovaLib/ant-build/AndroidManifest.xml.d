f4073e9de140d71c5ddaac00ea5de7b56de15505
davide <davide.macocchi@alten.it>
2014-08-27 20:03:55 +0200

    Remove all...

--- FRONTEND/StampIT/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/davide/Scrivania/alten/FRONTEND - StampIT/StampIT/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/home/davide/Scrivania/alten/FRONTEND - StampIT/StampIT/platforms/android/CordovaLib/AndroidManifest.xml \
--- FRONTEND/StampIT/platforms/android/CordovaLib/ant-gen/R.java.d
+++
@@ -1,1 +0,0 @@
- : /home/davide/Scrivania/alten/FRONTEND - StampIT/StampIT/platforms/android/CordovaLib/ant-build/AndroidManifest.xml \
