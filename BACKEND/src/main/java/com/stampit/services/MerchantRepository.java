0d0c4857f89b29933107fc68889746299b66e66a
Emilio Sellitto <emilio.selitto@alten.it>
2014-08-29 17:57:24 +0200

    Aggiunta query per ottenere la lista dei Merchants entro una certa distanza dal Customer in ordine di vicinanza.

--- BACKEND/src/main/java/com/stampit/services/MerchantRepository.java
+++ BACKEND/src/main/java/com/stampit/services/MerchantRepository.java
@@ -1,11 +1,23 @@
 package com.stampit.services;
 
+import java.util.List;
+
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.jpa.repository.Query;
+
 import com.stampit.model.Merchant;
+
+import javax.persistence.*;
 
 @RepositoryRestResource(collectionResourceRel ="merchant", path="merchants")
 public interface MerchantRepository extends PagingAndSortingRepository<Merchant, Long> {
+	@Query(value = "select m.*, st_distance(m.GPS_COORDINATES, geomFromText(:gpsCoordinates)) as calcDistance "
+				+ "from merchants as m "
+				+ "having calcDistance < :distance "
+				+ "order by calcDistance asc", nativeQuery = true)
+	List<Merchant> findByGpsCoordinates(@Param("gpsCoordinates")String gpsCoordinates, @Param("distance") float distance);
 }
 
 
